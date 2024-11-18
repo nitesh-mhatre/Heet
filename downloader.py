10745a71fcd2d5700ca3b3f35df949701d5a2cfc
nitesh-mhatre <n.mhatre90@gmail.com>
2024-11-18 21:49:03 +0530

    downloader created

--- downloader.py
+++ downloader.py
@@ -2,5 +2,13 @@ from src.groww import *
 
 import pandas as pd 
 
+expiry = ''
+symbol = "^NSEI"
 
+current_value = get_value(symbol)
 
+print("Live value is ",current_value)
+
+levels = get_levels(current_value, 50,6)
+
+print(levels)
